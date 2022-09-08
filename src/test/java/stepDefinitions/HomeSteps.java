529f23e24771ac984fd0f11aadcb8fe7ebf639bf
Robert Cabrera <clrobert.06@gmail.com>
2022-09-08 01:42:52 -0400

    feat(master): initial commit

---
+++ src/test/java/stepDefinitions/HomeSteps.java
@@ -0,0 +1,31 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import pages.HomePage;
+import utilities.DriverManager;
+
+public class HomeSteps {
+    private HomePage homePage = new HomePage(DriverManager.getInstance().getDriver());
+
+    @Then("I am in the home page")
+    @Then("I should be in the home page")
+    public void verifyImInHomePage() {
+        homePage.verifyHomePageHeaderIsDisplayed();
+    }
+
+    @Then("I verify that {string} is displayed")
+    public void verifyProductInHomePage(String product) {
+        homePage.verifyProductInHomePage(product);
+    }
+
+    @Then("I add sauce labs back pack product to the cart")
+    public void addSauceLabsBackPackToCart() {
+        homePage.clickOnAddSauceLabsBackPack();
+    }
+
+    @And("I click on the cart icon")
+    public void clickOnCartIcon() {
+        homePage.clickOnCartIcon();
+    }
+}
---
+++ src/test/resources/Home.feature
@@ -0,0 +1,16 @@
+Feature: As a user I should be able to see all the available items in the home page
+
+Background: User is logged into sauce demo web page
+  Given I set the user name text box with "standard_user"
+  And I set the password text box with "secret_sauce"
+  And I click on the login button
+
+  Scenario Outline: Products are available in the home page
+    When I am in the home page
+    Then I verify that "<product>" is displayed
+  Examples:
+    | product               |
+    | Sauce Labs Backpack   |
+    | Sauce Labs Bike Light |
+
+
